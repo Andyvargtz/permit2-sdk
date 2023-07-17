4f1f190c1630ea5d408834396a487082db5cdf87
Andrea Vargas <andrea.vargas@avalabs.org>
2023-07-17 14:54:48 -0600

    change address

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,6 @@
 import { BigNumber } from '@ethersproject/bignumber'
 
-export const PERMIT2_ADDRESS = '0x000000000022D473030F116dDEE9F6B43aC78BA3'
+export const PERMIT2_ADDRESS = '0xc7AC501814cF096EF536C03E40eef835Cfd407cC'
 
 export const MaxUint48 = BigNumber.from('0xffffffffffff')
 export const MaxUint160 = BigNumber.from('0xffffffffffffffffffffffffffffffffffffffff')
